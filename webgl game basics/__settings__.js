1ef4d83d891eaf9db3773bf3da2e6314dd7bc17f
Luntuntu <147110317+Luntuntu@users.noreply.github.com>
2024-07-29 20:19:15 -0400

    updated version with basic mechanics and animations

--- webgl game basics/__settings__.js
+++ webgl game basics/__settings__.js
@@ -8,7 +8,7 @@ window.CONTEXT_OPTIONS = {
     'deviceTypes': [`webgl2`, `webgl1`],
     'powerPreference': "default"
 };
-window.SCRIPTS = [ 187458253, 187458254, 187458255, 187458256 ];
+window.SCRIPTS = [ 187458253, 187458254, 187458255, 187458256, 188387940 ];
 window.CONFIG_FILENAME = "config.json";
 window.INPUT_SETTINGS = {
     useKeyboard: true,
