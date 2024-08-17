ae53b05b9395fe909e8547a995be72a6f0881617
Luntuntu <147110317+Luntuntu@users.noreply.github.com>
2024-08-16 23:34:57 -0400

    enemy behaviour
    change in enemy behaviour has affected animations a little bit as well, need to test for cause

---
+++ Sidescroller 2/__settings__.js
@@ -0,0 +1,22 @@
+window.ASSET_PREFIX = "";
+window.SCRIPT_PREFIX = "";
+window.SCENE_PATH = "2030517.json";
+window.CONTEXT_OPTIONS = {
+    'antialias': true,
+    'alpha': false,
+    'preserveDrawingBuffer': false,
+    'deviceTypes': [`webgl2`, `webgl1`],
+    'powerPreference': "default"
+};
+window.SCRIPTS = [ 187458253, 187458254, 187458255, 187458256, 188387940, 192478713, 192651201, 192652268 ];
+window.CONFIG_FILENAME = "config.json";
+window.INPUT_SETTINGS = {
+    useKeyboard: true,
+    useMouse: true,
+    useGamepads: false,
+    useTouch: true
+};
+pc.script.legacy = false;
+window.PRELOAD_MODULES = [
+    {'moduleName' : 'Ammo', 'glueUrl' : 'files/assets/187458277/1/ammo.wasm.js', 'wasmUrl' : 'files/assets/187458278/1/ammo.wasm.wasm', 'fallbackUrl' : 'files/assets/187458276/1/ammo.js', 'preload' : true},
+];
